9f42e0853402672585661f3eda0e91ac56b0d647
Paul Miller <paul@paulmillr.com>
2012-02-28 06:37:54 +0200

    Remove unused code.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,12 @@ require 'json'
 require 'lastfmtools'
 
 RSpec.configure do |config|
-  fixture_cache = {}
-
   def fixture_location
     File.join(File.dirname(__FILE__), 'fixtures')
   end
   
   def fixture(type)
     path = File.join(fixture_location, "#{type}.json")
-    File.open(path, 'r') do |file|
-      #fixture_cache[path] ||= 
-      JSON.parse(file.read)
-    end
+    File.open(path, 'r') { |file| JSON.parse(file.read) }
   end
 end
